c79fbee4a171096092bfd1a3d4cf721baf3aa731
luke.chen <luke.chen@MBP-DEV-01.local>
2017-07-21 13:54:46 +0800

    create

---
+++ CHPlayer/CHPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CHPlayer
+//
+//  Created by luke.chen on 2017/7/21.
+//  Copyright © 2017年 luke.chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
